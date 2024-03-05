71f168f7829e95dccaa9eed141d4873ab6e8b780
David Grove Jørgensen <dgj@kb.dk>
2024-03-05 14:04:53 +0100

    redirect to authentication on expired or missing token

--- bff/src/main/java/dk/kb/oauth/api/v1/impl/BffApiServiceImpl.java
+++ bff/src/main/java/dk/kb/oauth/api/v1/impl/BffApiServiceImpl.java
@@ -4,9 +4,13 @@ import dk.kb.oauth.OauthHelper;
 import dk.kb.oauth.ProxyHelper;
 
 import dk.kb.oauth.api.v1.BffApi;
+import dk.kb.util.webservice.exception.InternalServiceException;
 import dk.kb.util.webservice.exception.ServiceException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.keycloak.TokenVerifier;
+import org.keycloak.common.VerificationException;
+import org.keycloak.representations.AccessToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +24,7 @@ import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.time.Instant;
 
 
 /**
@@ -43,8 +48,15 @@ public class BffApiServiceImpl extends ImplBase implements BffApi {
     @Path("/proxy/{api}/{path: .*}")
     public StreamingOutput proxyGetRequest(@PathParam("api") String api, @PathParam("path") String path, @CookieParam("Authorization") String authorization) {
         if (StringUtils.isEmpty(authorization)) {
-            throw new ServiceException(Response.Status.UNAUTHORIZED);
+            sendRedirectToAuthentication();
+            return null;
         }
+        String accessTokenString = authorization;
+        if (!verifyAccessTokenString(accessTokenString)) {
+            sendRedirectToAuthentication();
+            return null;
+        }
+
         try {
             URI uri = ProxyHelper.getApiUri(api, path, uriInfo.getRequestUri().getRawQuery());
             HttpURLConnection apiConnection = ProxyHelper.openConnection("GET", uri, httpHeaders, authorization);
@@ -55,6 +67,25 @@ public class BffApiServiceImpl extends ImplBase implements BffApi {
         } catch (IOException e) {
             log.error("IOEXception",e);
             throw new ServiceException(Response.Status.BAD_GATEWAY);
+        }
+    }
+
+    private void sendRedirectToAuthentication() {
+        try {
+            httpServletResponse.sendRedirect(uriInfo.getBaseUri()+"/authentication");
+        } catch (IOException e) {
+            log.error("Error sending redirect",e);
+            throw new InternalServiceException();
+        }
+    }
+
+    private boolean verifyAccessTokenString(String accessTokenString) {
+        try {
+            AccessToken accessToken = TokenVerifier.create(accessTokenString, AccessToken.class).getToken();
+            return Instant.now().getEpochSecond() > accessToken.getExp() - 60;
+        } catch (VerificationException e) {
+            log.error("Unable to parse access token ", e);
+            throw new InternalServiceException();
         }
     }
 }
