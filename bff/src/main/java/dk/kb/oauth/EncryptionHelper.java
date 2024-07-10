e3f92498878ce85828a1479b8f0664f6f668dba0
David Grove Jørgensen <dgj@kb.dk>
2024-07-10 09:33:00 +0200

    DRA-1031 Switch encryption algorithm to AES/GCM/NoPadding

--- bff/src/main/java/dk/kb/oauth/EncryptionHelper.java
+++ bff/src/main/java/dk/kb/oauth/EncryptionHelper.java
@@ -6,16 +6,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.crypto.*;
-import java.io.UnsupportedEncodingException;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
 public class EncryptionHelper {
+    //TODO: should these be moved to config
+    private static final int AES_KEY_SIZE = 256;
+    private static final int INITIALIZATION_VECTOR_LENGTH = 16;
+    private static final String CIPHER_ALGORITHM = "AES/GCM/NoPadding";
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String FACTORY_INSTANCE = "PBKDF2WithHmacSHA512";
 
     private static final Logger log = LoggerFactory.getLogger(EncryptionHelper.class);
+
+    private static final SecretKeyFactory keyFactory;
+
+    static {
+        try {
+            keyFactory = SecretKeyFactory.getInstance(FACTORY_INSTANCE);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * Encrypt a string using AES and the configured secret salt
@@ -26,12 +47,15 @@ public class EncryptionHelper {
     public static String encryptString(String plain) {
         try {
             SecretKey key = generateKey(ServiceConfig.getConfig().getString("secretSalt"));
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] iv = getRandomBytes(INITIALIZATION_VECTOR_LENGTH);
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(INITIALIZATION_VECTOR_LENGTH * 8, iv));
             byte[] encryptedBytes = cipher.doFinal(plain.getBytes(StandardCharsets.UTF_8));
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (UnsupportedEncodingException | NoSuchPaddingException | IllegalBlockSizeException |
-                 NoSuchAlgorithmException | BadPaddingException | InvalidKeyException e) {
+            byte[] cipherBytes = ByteBuffer.allocate(iv.length+encryptedBytes.length)
+                .put(iv).put(encryptedBytes).array();
+            return Base64.getEncoder().encodeToString(cipherBytes);
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException |
+                 InvalidKeyException | InvalidKeySpecException | InvalidAlgorithmParameterException e) {
             log.error("encryption error ",e);
             throw new InternalServiceException("encryption error");
         }
@@ -45,24 +69,35 @@ public class EncryptionHelper {
      */
     public static String decryptString(String encryptedString){
         try {
-            SecretKey key = generateKey(ServiceConfig.getConfig().getString("secretSalt"));
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] encryptedBytes = Base64.getDecoder().decode(encryptedString);
-            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedBytes);
+            byte[] iv = new byte[INITIALIZATION_VECTOR_LENGTH];
+            byteBuffer.get(iv);
+            byte[] content = new byte[byteBuffer.remaining()];
+            byteBuffer.get(content);
+
+            SecretKey key = generateKey(ServiceConfig.getConfig().getString("secretSalt"));
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key,  new GCMParameterSpec(INITIALIZATION_VECTOR_LENGTH * 8, iv));
+            byte[] decryptedBytes = cipher.doFinal(content);
             return new String(decryptedBytes,StandardCharsets.UTF_8);
-        } catch (IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
+        } catch (InvalidAlgorithmParameterException  |InvalidKeySpecException | IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
             log.error("decryption error ",e);
             throw new InternalServiceException("decryption error");
         }
     }
 
-    private static SecretKey generateKey(String salt) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-        secureRandom.setSeed(salt.getBytes(StandardCharsets.UTF_8));
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(256,secureRandom);
-        return keyGenerator.generateKey();
+    private static SecretKey generateKey(String secretSalt) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(secretSalt.toCharArray(), secretSalt.getBytes(StandardCharsets.UTF_8), 65536, AES_KEY_SIZE);
+        SecretKey pbeKey = keyFactory.generateSecret(pbeKeySpec);
+        return new SecretKeySpec(pbeKey.getEncoded(), KEY_ALGORITHM);
+    }
+
+    private static byte[] getRandomBytes(int lengh) {
+        byte[] bytes = new byte[lengh];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(bytes);
+        return bytes;
     }
 
 }
