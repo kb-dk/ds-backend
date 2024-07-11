cc29b5cc00ca7373ab1f9e2947ee1b33f127d153
David Grove Jørgensen <dgj@kb.dk>
2024-07-11 09:38:17 +0200

    DRA-1033 added test

--- bff/src/test/java/dk/kb/oauth/EncryptionHelperTest.java
+++ bff/src/test/java/dk/kb/oauth/EncryptionHelperTest.java
@@ -2,19 +2,23 @@ package dk.kb.oauth;
 
 import dk.kb.oauth.config.ServiceConfig;
 import dk.kb.util.Resolver;
+import dk.kb.util.webservice.exception.InternalServiceException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EncryptionHelperTest {
 
@@ -25,11 +29,24 @@ public class EncryptionHelperTest {
     }
 
     @Test
-    public void testEncryptDecrypt() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException {
+    public void testEncryptDecrypt() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, IOException {
         String testString = "abcdefg12345678";
         String encryptedString =  EncryptionHelper.encryptString(testString);
         String decryptedString = EncryptionHelper.decryptString(encryptedString);
         assertEquals(testString,decryptedString);
+    }
+
+    @Test
+    public void testSaltChange() throws IOException {
+        String testString = "abcdefg12345678";
+        String encryptedString =  EncryptionHelper.encryptString(testString);
+        String decryptedString = EncryptionHelper.decryptString(encryptedString);
+        assertEquals(testString,decryptedString);
+        Path knownFile = Path.of(Resolver.resolveURL("bff-test-newsalt.yaml").getPath());
+        ServiceConfig.getInstance().initialize(knownFile.toString());
+        assertEquals(testString,decryptedString);
+        assertThrows(InternalServiceException.class, () ->
+            EncryptionHelper.decryptString(encryptedString));
     }
 
 }
