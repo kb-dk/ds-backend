6f1603703dd62acd885417005e61571bc3060cc0
seskildsen <snes@kb.dk>
2026-09-08 16:06:25 +0200

    style: add indents

--- ds-storage/src/main/resources/db/migration/ds-storage/V1__init.sql
+++ ds-storage/src/main/resources/db/migration/ds-storage/V1__init.sql
@@ -1,18 +1,18 @@
 -- V1__init.sql
 
 CREATE TABLE ds_records (
-id VARCHAR(255) PRIMARY KEY,
-origin VARCHAR(31),
-orgid VARCHAR(255),
-id_error INTEGER,
-deleted INTEGER,
-data TEXT,
-ctime BIGINT,
-mtime BIGINT,
-parentid VARCHAR(255),
-recordtype VARCHAR(31),
-referenceid VARCHAR(255),
-kalturaid VARCHAR(255)
+    id VARCHAR(255) PRIMARY KEY,
+    origin VARCHAR(31),
+    orgid VARCHAR(255),
+    id_error INTEGER,
+    deleted INTEGER,
+    data TEXT,
+    ctime BIGINT,
+    mtime BIGINT,
+    parentid VARCHAR(255),
+    recordtype VARCHAR(31),
+    referenceid VARCHAR(255),
+    kalturaid VARCHAR(255)
 );
 
 CREATE UNIQUE INDEX i ON ds_records(id);
@@ -28,12 +28,13 @@ CREATE INDEX kalid ON ds_records (kalturaid);
 
 
 CREATE TABLE transcriptions (
-fileid VARCHAR(255) PRIMARY KEY,
-filename VARCHAR(255),
-mtime BIGINT,
-transcription TEXT,
-transcription_lines TEXT
+    fileid VARCHAR(255) PRIMARY KEY,
+    filename VARCHAR(255),
+    mtime BIGINT,
+    transcription TEXT,
+    transcription_lines TEXT
 );
+
 CREATE UNIQUE INDEX fileid_trans ON transcriptions(fileid);
 CREATE UNIQUE INDEX m_trans ON transcriptions(mtime);
 
